1dad59d46008d3bff3d7f780767a88322fc3b2d2
pszer <pszerszynski256@gmail.com>
2026-09-22 05:47:36 +0200

    commit

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # storyboard-helper
 
-wip
+**storyboard-helper** is a DSL compiler that lets you write **.osb** format osu! storyboards in **Lua**.
+
+It aims to make animating and scripting storyboards easier in three ways:
+
+1. Relative, combinable transformations.
+2. Metaprogramming support.
+3. Automatic optimisation.
+
+In the .osb format all the transformation commands work in absolutes, you can tell a sprite to
+move from one specific screen co-ordinate to another, but you can't tell it to move 50 pixels
+to the right from where it currently is, same with scales, rotations and colour. When scripting
+you have to manually track the transformation states of objects.
+**storyboard-helper** has relative versions of each of the .osb transformations, so moving
+a sprite 50 pixels from to the right becomes the command:
+
+`{'MoveRel', 'linear', {'00:01:000', '00:02:000'}, {0,0}, {50, 50}}`
+
+Relative commands have no limits to how they can overlap in time, and even with non-linear easings their
+expected motion will be resolved and keyframed in the end.
+
+With relative commands, there are option for more abstract and advanced ways of scripting animations.
+Custom, compounded and potentially recursive commands can be created in a functional programming manner,
+or defined by the user and added to the compiler, where they will integrate with the rest of the
+compilation steps and can be used with any other parts/commands of the program such as the *keyframer*.
+
+## Optimisation and documentation WIP
--- init.lua
+++ init.lua
@@ -5,18 +5,6 @@ local sb = require 'storyboard'
 
 --local testobj = object:new("slidez.png", "Background", "TopLeft", 320, 240)
 
-local function do_the_wiggly_worm_lol (t,dur,period)
-	return function(out)
-		local P = period/4.0
-		for i=t,dur,period do
-			out{"moverel", "sineIn"  , {i    , i+P}, {0,0}, {0,15}}
-			out{"moverel", "sineOut" , {i+P  , i+P*2}, {0,0}, {0,0}}
-			out{"moverel", "sineIn"  , {i+P*2, i+P*3}, {0,0}, {0,-15}}
-			out{"moverel", "sineOut" , {i+P*3, i+P*4}, {0,0}, {0,0}}
-		end
-	end
-end
-
 --[[
 print(
  object:new("slidez.png", "Background", "Center", 320, 240):out(
@@ -41,23 +29,29 @@ print(
 	 --{"moverel", "linear", {'-0:00:500', '0:01:500'}, {10,10}, {50,50}}
  ))--]]
 
-
+local function wiggle(t,dur,period)
+	return function(out)
+		local P = period/4.0
+		for i=t,dur,period do
+			out{"moverel", "sineIn"  , {i    , i+P}, {0,0}, {0,15}}
+			out{"moverel", "sineOut" , {i+P  , i+P*2}, {0,0}, {0,0}}
+			out{"moverel", "sineIn"  , {i+P*2, i+P*3}, {0,0}, {0,-15}}
+			out{"moverel", "sineOut" , {i+P*3, i+P*4}, {0,0}, {0,0}}
+		end
+	end
+end
 storyboard = sb:new("./sb.osb")
-
 storyboard:newObject("slidez.png", "Background", "Center", 320, 240):add(
 	{"move" , "linear"     , {0, 5000}, {100,100}, {200,300}},
 	{"moverel", "elasticout" , {1000, 2000}, {0,0}, {100,-10}},
 	{"moverel", "elasticout" , {3000, 4000}, {0,0}, {100,-10}},
 	{"moverel", "backout"    , {5000, 6000}, {0,0}, {0,-200}},
 	{"move" , "linear"     , {8900, 10000}, {400,240}, {100,240}},
-
 	{"moverel", "elasticout" , {7000,8400}, {0,0}, {-50,5}},
 	{"moverel", "elasticout" , {7900,8900}, {0,0}, {-30,-10}},
 	{"move" , "linear"     , {8900, 10000}, {400,240}, {100,240}},
-	{"move" , "linear"     , {10000, 11000}, {100,240}, {320,40}},--]]
-
-	do_the_wiggly_worm_lol(8900, 11000, 500),--]]
-
+	{"move" , "linear"     , {10000, 11000}, {100,240}, {320,40}},
+	wiggle(8900, 11000, 500),
 	{"vector" , "linear", {0, 2000}, {1,1}, {-1,-1}},
 	{"vector" , "linear", {3000, 5000}, {1,1}, {-1,-1}},
 	{"vector" , "linear", {6000, 8000}, {2,2}, {-1,-1}},
@@ -65,6 +59,4 @@ storyboard:newObject("slidez.png", "Background", "Center", 320, 240):add(
 	{"scalerel" , "elasticout", {1000, 1500}, {1.0}, {1.5}},
 	{"vector" , "linear", {12000, 13000}, {-1,-1}, {-2,-2}}
 )
-
---print(storyboard:out())
 storyboard:writeToFile()
